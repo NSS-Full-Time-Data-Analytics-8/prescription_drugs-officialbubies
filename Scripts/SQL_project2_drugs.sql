e064e856833fbaa42b22c5da389ed24f761de8f5
officialbubies <sebastianbuberry@gmail.com>
2022-09-27 15:55:37 -0500

    initial push drugs 1-5

--- Scripts/SQL_project2_drugs.sql
+++ Scripts/SQL_project2_drugs.sql
@@ -34,7 +34,44 @@ INNER JOIN prescriber
 USING(npi)
 ORDER BY total_claim_count ASC;
 
---q3 a) 
+/* q3: a) INSULIN GLARGINE,HUM.REC.ANLOG
+	   b) C1 ESTERASE INHIBITOR
+*/
+SELECT generic_name, ROUND(SUM(total_drug_cost), 2) AS highest_cost
+FROM drug
+INNER JOIN prescription
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY highest_cost DESC;
+
+SELECT generic_name, ROUND(SUM(total_drug_cost)/SUM(total_day_supply), 2) AS cost_per_day
+FROM drug
+INNER JOIN prescription
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY cost_per_day DESC;
+
+-- q4 a)
+SELECT DISTINCT drug_name,
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opiod' 
+		 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		 ELSE 'neither' END AS drug_type
+FROM drug
+ORDER BY drug_name;
+
+-- q4 b)
+SELECT DISTINCT drug_name, total_drug_cost::money,
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opiod' 
+		 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		 ELSE 'neither' END AS drug_type
+FROM drug
+INNER JOIN prescription
+USING(drug_name)
+ORDER BY total_drug_cost DESC;
+
+--q5
+
+
 
 
 
